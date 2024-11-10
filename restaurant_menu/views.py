473ad4065a847f90a98756400da528e57925302f
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-11-10 14:49:04 +0200

    435. Context in Django

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from .models import Item
@@ -6,6 +7,10 @@ from .models import Item
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("meal_type", "meal")
     template_name = "index.html"
+
+    def get_context_data(self):
+        context = {"meals": "Pizza"}
+        return context
 
 
 class MenuItemDetail(generic.DetailView):
